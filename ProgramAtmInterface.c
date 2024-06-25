49b23cbfb89a2967d080571f5a0fa2d987941f6d
Watcharapong Namsaeng (KPS MAIL) <47651@kalasinpit.ac.th>
2024-06-25 18:49:36 +0700

    Create ProgramAtmInterface.c

---
+++ ProgramAtmInterface.c
@@ -0,0 +1,21 @@
+/*
+C PROGRAM (BASIC)
+INTERFACE : PROGRAM ATM
+*/
+
+#include <stdio.h>
+
+void main(){
+    printf("\
+    *****************************\n\
+    \tProgram ATM\n\
+    *****************************\n\n\
+    1> Deposit\n\
+    2> Withdraw\n\
+    3> Transfer\n\
+    4> View account balance\n\
+    5> Exit the program\n\
+    *****************************\n\
+    Please select menu [1-5]:");
+    return;
+}
