204ea18dfc8eba0b5a0ea1c3b022ff754ae4f5b8
Watcharapong Namsaeng (KPS E-MAIL) <47651@kalasinpit.ac.th>
2024-07-01 16:59:45 +0700

    Create ProgramBmi.c

---
+++ ProgramBmi.c
@@ -0,0 +1,24 @@
+/*
+C PROGRAM (BASIC)
+INTERFACE : BMI
+DATE : 01/07/2567
+*/
+
+#include <stdio.h>
+
+int main() {
+    printf("\
+    *********************\n\
+    \tProgram BMI\n\
+    *********************\n");
+
+    float input[2];
+    printf("input weight (kg): ");
+    scanf("%f", &input[0]);
+    printf("input height (cm): ");
+    scanf("%f", &input[1]);
+
+    float calculate = input[0] / ((input[1] / 100) * (input[1] / 100));
+    printf("BMI : %.2f\n", calculate);
+    return;
+}
